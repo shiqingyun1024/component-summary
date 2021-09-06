1cd63227cc2fd6a55d35d3a084b2433508b68e48
814657780 <18037818812@163.com>
2021-09-07 07:40:22 +0800

    异常监控处理报错信息

--- 前端异常埋点系统/error-point-system/plugins/uploadErr.ts
+++ 前端异常埋点系统/error-point-system/plugins/uploadErr.ts
@@ -1,4 +1,4 @@
-let uploadErr = function({ lineno, colno, error: { stack }, message, filename }) {
+let uploadErr = function ({ lineno, colno, error: { stack }, message, filename }) {
     let str = window.btoa(
         JSON.stringify({
             lineno,
@@ -12,7 +12,7 @@ let uploadErr = function({ lineno, colno, error: { stack }, message, filename })
     // let front_ip = "http://location:3000/error";
     new Image().src = `${front_ip}?info=${str}`;
 }
-let uploadErr2 = function(stack) {
+let uploadErr2 = function (stack) {
     let str = window.btoa(
         JSON.stringify(stack)
     );
@@ -21,4 +21,4 @@ let uploadErr2 = function(stack) {
     new Image().src = `${front_ip}?info=${str}`;
 }
 
-export {uploadErr,uploadErr2}
+export { uploadErr, uploadErr2 }
